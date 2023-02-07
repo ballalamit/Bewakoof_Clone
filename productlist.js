854376a4a374233bf0a3cd1b40a5af2d1cc04459
Amit B <ballalamit22gmail.com>
2023-02-07 13:24:57 +0530

    crated the database

---
+++ productlist.js
@@ -0,0 +1,352 @@
+
+
+var mydatabase= [
+    {
+        id: 1,
+        image_url: "https://images.bewakoof.com/t640/men-s-black-oversized-sweatshirt-555427-1671801629-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Black Oversized Sweatshirt",
+        price:764,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "sweatshirt",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.3
+    },
+    {
+        id: 2,
+        image_url: "https://images.bewakoof.com/t640/men-s-blue-zipper-hoodie-363707-1672410888-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Zipper Hoodie",
+        price:764,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 3,
+        image_url: "https://images.bewakoof.com/t640/men-s-teal-blue-hoodie-499039-1674553104-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Teal Blue Hoodie",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 4,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 5,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 6,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 7,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+
+    {
+        id: 8,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+    {
+        id: 9,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+    {
+        id: 10,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+    {
+        id: 4,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+    {
+        id: 11,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    },
+    {
+        id: 12,
+        image_url: "https://images.bewakoof.com/t640/men-0-05-goku-badge-printed-sweatshirt-551684-1668513057-1.jpg", 
+        brand: "Bewakoof",
+        name: "Men's Blue Goku Badge Printed Sweatshirt",
+        price:899,
+        strikeprice: 1099,
+        tribememprice: 699,
+        fav: false,
+        //filtered keys and value
+        type: "hoodie",
+        sizes : {
+            small: true,
+            medium: true,
+            large: true
+        },
+        color :{
+            black: true,
+            blue: true,
+            green: true,
+            yellow: true,
+        },
+        rating:4.6,
+    }
+
+]
+
+    // mydatabase.map(function(elem, index){
+    // console.log(elem.image_url)
+    // })
+
+
+    displayProducts();
+    function displayProducts(){
+        mydatabase.map(function(elem, index){
+            var div= document.createElement("div");
+            div.setAttribute("class", "container");
+
+            var img= document.createElement("img");
+            img.setAttribute("src",elem.image_url)
+            img.setAttribute("class", "img")
+
+            var h3= document.createElement("h3");
+            h3.textContent=elem.name;
+
+            var p=document.createElement("p");
+            p.textContent=elem.price;
+
+            var strikeprice=document.createElement("p");
+            strikeprice.textContent=elem.strikeprice;
+
+            div.append(img, h3, p, strikeprice);
+            document.querySelector("#right").append(div);
+        })
+    }
